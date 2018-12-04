e7a8c08394bc3619c71948a725d0789ea807dab2
bibek shrestha <bibekmanshrestha@gmail.com>
2018-12-03 22:48:27 -0500

    fix accordion block issue in menu.html and updated links in navs

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,17 +2,20 @@ $(document).ready(function() {
    $('.accordion-text').hide();
 
     $('#menu-accordion h3').click(function() {
-        if ($(this).next('div')[0].style['display'] === 'block') {
-            $(this).next('div').hide(400, 'swing');
-            return;
+        if ($(this).next('div')[0].style['display'] === 'none') {
+            $('#menu-accordion>div').hide(400, 'swing');
+            $(this).next('div').show(400, 'swing');
+            // return;
         }
-        $('#menu-accordion>div').hide(400, 'swing');
-        $(this).next('div').show(400, 'swing');
+        else {
+            $(this).next('div').hide(400, 'swing');
+        }
+        
     })
 
-    $('body').click(function(event) {
-        if (!$(event.target).hasClass('accordion-block')) {
-            $('#menu-accordion>div').hide(400, 'swing');
-        }
-    })
+    // $('body').click(function(event) {
+    //     if (!$(event.target).hasClass('accordion-block')) {
+    //         $('#menu-accordion>div').hide(400, 'swing');
+    //     }
+    // })
 })
